074f0b7f9727cbff73fcf79f8be5a5dff8d2dacf
shokohara <shokohara@users.noreply.github.com>
2019-01-16 00:33:14 +0900

    .

--- slackworkcount/src/Main.hs
+++ slackworkcount/src/Main.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-} 
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Main where
 
-import qualified Network.Slack.Api as Slack
-import Network.Slack.Request
+import qualified Web.Slack as Slack
+import qualified Web.Slack.Api as Api
+import Control.Monad.Reader
+-- import qualified Network.Slack.Api as Slack
 import Data.String (fromString)
 import System.Environment (getEnv)
 import qualified Data.ByteString.Lazy as L
@@ -14,11 +17,14 @@ import Data.Aeson.Casing
 import Data.Either
 import Data.String.Conversions
 import Control.Monad
-import Data.Time.Clock
+import Data.Time.LocalTime
+import Data.Time.Format (parseTime)
+import Data.Time.Locale.Compat (defaultTimeLocale)
+import Data.Text (Text, pack, unpack)
 
 data Message = Message {
   --messageClientMsgId :: String,
-  messageText :: String, messageUser :: String, messageTs :: UTCTime } deriving (Show, Generic)
+  messageText :: String, messageUser :: String, messageTs :: MyLocalTime } deriving (Show, Generic)
 data Response = Response { responseOk :: Bool, responseMessages :: [Message], responseHasMore :: Bool, responseIsLimited :: Bool } deriving Generic
 
 instance FromJSON Message where
@@ -27,12 +33,13 @@ instance FromJSON Message where
 instance FromJSON Response where
   parseJSON = genericParseJSON $ aesonPrefix snakeCase
 
-instance FromJSON UTCTime where
-  parseJSON = withText "UTCTime" $ \t ->
-        case parseTime defaultTimeLocale "%FT%T%QZ" (unpack t) of
-          Just d -> pure d
-          _      -> fail "could not parse ISO-8601 date"
+newtype MyLocalTime = MyLocalTime { a :: LocalTime } deriving (Show, Generic)
 
+instance FromJSON MyLocalTime where
+  parseJSON = withText "LocalTime" $ \t ->
+        case parseTime defaultTimeLocale "%FT%T%QZ" (unpack t) of
+          Just d -> pure $ MyLocalTime d
+          _      -> fail "could not parse ISO-8601 date"
 
 token :: IO Token
 token = fromString <$> getEnv "TOKEN"
@@ -52,9 +59,10 @@ channels token = do
 
 main :: IO ()
 main = do
-  t <- token
-  res <- channels t
-  forM_ (filter (\a -> messageUser a == "UELRGL7RR") res) print
+  slackConfig <- Slack.mkSlackConfig token
+--  res <- channels slackConfig
+  flip runReaderT slackConfig (Slack.apiTest Api.mkTestReq)
+--  forM_ (filter (\a -> messageUser a == "UELRGL7RR") res) print
 --  print (ts <$> res)
   putStrLn "hello world"
 
