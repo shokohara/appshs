a6da3ddb858e3fedddc2aad95e7af36d13caf4e5
shokohara <shokohara@users.noreply.github.com>
2019-01-17 01:42:37 +0900

    .

--- synczshhistry/src/Main.hs
+++ synczshhistry/src/Main.hs
@@ -1,5 +1,11 @@
 module Main where
 
+import Data.Time.Clock.POSIX
+import Data.Time.Format
+
+data History = History 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  str <- readFile "/Users/sho/zsh_history/histories"
+  print $ formatTime defaultTimeLocale  "%c" $ posixSecondsToUTCTime 10
+  putStrLn str
