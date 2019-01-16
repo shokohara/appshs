241942597973973b7928ae540fc2d2eaf3287920
shokohara <shokohara@users.noreply.github.com>
2019-01-16 21:35:18 +0900

    .

--- slackworkcount/src/Main.hs
+++ slackworkcount/src/Main.hs
@@ -6,6 +6,7 @@ module Main where
 
 import qualified Web.Slack as Slack
 import qualified Web.Slack.Api as Api
+import qualified Web.Slack.Common as Api
 import Control.Monad.Reader
 -- import qualified Network.Slack.Api as Slack
 import Data.String (fromString)
@@ -27,41 +28,37 @@ data Message = Message {
   messageText :: String, messageUser :: String, messageTs :: MyLocalTime } deriving (Show, Generic)
 data Response = Response { responseOk :: Bool, responseMessages :: [Message], responseHasMore :: Bool, responseIsLimited :: Bool } deriving Generic
 
-instance FromJSON Message where
-  parseJSON = genericParseJSON $ aesonPrefix snakeCase
-
-instance FromJSON Response where
-  parseJSON = genericParseJSON $ aesonPrefix snakeCase
-
+-- instance FromJSON Message where
+--   parseJSON = genericParseJSON $ aesonPrefix snakeCase
+-- 
+-- instance FromJSON Response where
+--   parseJSON = genericParseJSON $ aesonPrefix snakeCase
+-- 
 newtype MyLocalTime = MyLocalTime { a :: LocalTime } deriving (Show, Generic)
 
-instance FromJSON MyLocalTime where
-  parseJSON = withText "LocalTime" $ \t ->
-        case parseTime defaultTimeLocale "%FT%T%QZ" (unpack t) of
-          Just d -> pure $ MyLocalTime d
-          _      -> fail "could not parse ISO-8601 date"
-
-token :: IO Token
-token = fromString <$> getEnv "TOKEN"
-
-channels :: Token -> IO [Message]
-channels token = do
-  res <- Slack.channelsHistory token [("channel", "C04L83KET")]
-  putStrLn (cs $ g res)
-  return . responseMessages . b . g $ res
-  where
-    g :: Slack.SlackResponse -> L.ByteString
-    g a = case a of
-      Slack.InvalidEndpoint -> error ""
-      Slack.Success a -> a
-    b :: L.ByteString -> Response
-    b = either error id . eitherDecode
+--channels :: Token -> IO [Message]
+--channels token = do
+--  res <- Slack.channelsHistory token [("channel", "C04L83KET")]
+--  putStrLn (cs $ g res)
+--  return . responseMessages . b . g $ res
+--  where
+--    g :: Slack.SlackResponse -> L.ByteString
+--    g a = case a of
+--      Slack.InvalidEndpoint -> error ""
+--      Slack.Success a -> a
+--    b :: L.ByteString -> Response
+--    b = either error id . eitherDecode
 
 main :: IO ()
 main = do
-  slackConfig <- Slack.mkSlackConfig token
+  token <- getEnv "TOKEN"
+  slackConfig <- Slack.mkSlackConfig (fromString token)
 --  res <- channels slackConfig
-  flip runReaderT slackConfig (Slack.apiTest Api.mkTestReq)
+  a <- flip runReaderT slackConfig (Slack.apiTest Api.mkTestReq)
+  print a
+  print $ Api.mkHistoryReq "C04L83KET"
+  b <- flip runReaderT slackConfig (Slack.channelsHistory $ Api.mkHistoryReq "C04L83KET")
+  print b
 --  forM_ (filter (\a -> messageUser a == "UELRGL7RR") res) print
 --  print (ts <$> res)
   putStrLn "hello world"
